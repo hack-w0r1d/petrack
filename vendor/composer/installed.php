fb379646eab3dfd92ca2054a32afc1a0839364d1
Y M <ciel@Ciels-MacBook-Pro.local>
2025-08-13 01:24:31 +0900

    20250812 - Eloquentリレーション - 実装中

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd73985620b138bcab77c97d1e0a1260c238fc46d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd73985620b138bcab77c97d1e0a1260c238fc46d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
