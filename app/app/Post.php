c140601e3618789f893651d8ec113bcbecf8837d
Y M <ciel@Ciels-MacBook-Pro.local>
2025-08-16 03:07:22 +0900

    20250815 - 認証機能 - 実装中

--- app/app/Post.php
+++ app/app/Post.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\User;
 use App\Tag;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
     public function user()
     {
         return $this->belongsTo(User::class);
---
+++ app/database/migrations/2025_08_11_090119_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('pet_id')->nullable()->constrained('pets')->nullonDelete();
+            $table->string('image_path');
+            $table->text('caption')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
