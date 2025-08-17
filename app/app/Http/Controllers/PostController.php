c98c4e9a1b203bbab52627b857539676f61d76d9
Y M <ciel@Ciels-MacBook-Pro.local>
2025-08-18 05:39:08 +0900

    20250818 - ポリシークラス - 実装中

--- app/app/Http/Controllers/PostController.php
+++ app/app/Http/Controllers/PostController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function edit(Post $post)
+    {
+        $this->authorize('update', $post);  // 自身の投稿以外は403エラー
+        return view('');
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete', $post);  // 自身の投稿以外は403エラー
+        // $post->delete();
+        return redirect()->route('');
+    }
 }
