2d9bbd51b1737f0eb119b7513f63988f148041ae
Y M <ciel@Ciels-MacBook-Pro.local>
2025-09-07 07:01:20 +0900

    20250907 - いいね機能 - 完了

--- app/database/migrations/2025_09_06_153922_create_likes_table.php
+++ app/database/migrations/2025_09_06_153922_create_likes_table.php
@@ -22,7 +22,7 @@ class CreateLikesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
 
-            $table->unique('user_id', 'post_id');
+            $table->unique(['user_id', 'post_id']);
         });
     }
 
